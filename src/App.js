808f792232e34983ddfc6042c38652e8cd1d925e
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-03-31 09:28:47 -0700

    Step 4

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ import ProtectedRoute from './components/ProtectedRoute';
 import React from 'react';
 import Restaurant from "./components/Restaurant";
 import Restaurants from "./components/Restaurants";
-import UserMenuRoute from "./routes/UserMenuRoute";
+import SignInRoute from "./routes/SignInRoute";
+import SignUpRoute from "./routes/SignUpRoute";
 
-//import SignInRoute from "./routes/SignInRoute";
-
-
-//import SignUpRoute from "./routes/SignUpRoute";
+//import UserMenuRoute from "./routes/UserMenuRoute";
 
 
 //import AddDish from './components/Admin/AddDish';
@@ -32,9 +30,9 @@ function App() {
 
 					<Route path="/" element={<Restaurants />} />
 					<Route path="/:id" element={<Restaurant />} />
-					{/*<Route path="/auth/login" element={<SignInRoute />} />
-					<Route path="/signup" element={<SignUpRoute />} />*/}
-					<Route path="/usermenu" element={<UserMenuRoute />} />
+					<Route path="/auth/login" element={<SignInRoute />} />
+					<Route path="/signup" element={<SignUpRoute />} />
+					{/*<Route path="/usermenu" element={<UserMenuRoute />} />*/}
 				</Routes>
 			</main>
 		</Router>

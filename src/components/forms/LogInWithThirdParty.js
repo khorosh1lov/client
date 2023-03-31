b7bfa9e148cbf0aa9fde31146a1e51a126fd7451
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-03-31 10:30:45 -0700

    LoginThirdParty last check

--- src/components/forms/LogInWithThirdParty.js
+++ src/components/forms/LogInWithThirdParty.js
@@ -1,36 +1,37 @@
-//import { BsFacebook, BsApple, BsGoogle } from "react-icons/bs"; 
-//-------------BAD LIBRARY---------------//
+//-------------BAD LIBRARY WITH MEMORY LEAKING---------------//
 
-import { IconContext } from "react-icons";
+import { BsApple, BsFacebook, BsGoogle } from "react-icons/bs";
+
+import { IconContext } from 'react-icons';
+
+//-------------BAD LIBRARY WITH MEMORY LEAKING---------------//
+
+
 
 function LoginWithThirdParty() {
-  return (
-    <>
-      <div className="col-md-8">
-        <hr></hr>
-      </div>
-      <button type="submit" className="col-md-8 mb-2 btn btn-info text-light">
-        <IconContext.Provider
-          value={{
-            color: "white",
-            className: "global-class-name",
-            size: "1.1em",
-          }}
-        >
-          {/* <BsGoogle /> */}
-        </IconContext.Provider>{" "}
-        Continue with Google
-      </button>
-      <button
-        type="submit"
-        className="col-md-8 mb-2 btn btn-primary text-light"
-      >
-        {/* <BsFacebook /> */} Continue with Facebook
-      </button>
-      <button type="submit" className="col-md-8 mb-2 btn btn-dark text-light">
-        {/* <BsApple /> */} Continue with Apple
-      </button>
-    </>
-  );
+	return (
+		<>
+			<div className="col-md-8">
+				<hr></hr>
+			</div>
+			<button type="submit" className="col-md-8 mb-2 btn btn-info text-light">
+				<IconContext.Provider
+					value={{
+						color: 'white',
+						className: 'global-class-name',
+						size: '1.1em',
+					}}>
+					<BsGoogle />
+				</IconContext.Provider>{' '}
+				Continue with Google
+			</button>
+			<button type="submit" className="col-md-8 mb-2 btn btn-primary text-light">
+				<BsFacebook /> Continue with Facebook
+			</button>
+			<button type="submit" className="col-md-8 mb-2 btn btn-dark text-light">
+				<BsApple /> Continue with Apple
+			</button>
+		</>
+	);
 }
 export default LoginWithThirdParty;

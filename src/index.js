df3aca401c726c8a6660da405d7109f95848bccf
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-04-03 15:52:59 -0700

    PWA integration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+		navigator.serviceWorker
+			.register(publicUrl.pathname + 'service-worker.js')
+			.then((registration) => {
+				console.log('Service Worker registered: ', registration);
+			})
+			.catch((error) => {
+				console.error('Service Worker registration failed: ', error);
+			});
+	});
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
